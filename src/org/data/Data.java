d6481540eae6005a887b823d97ae3670410f3364
SivaNandhini4997 <nandynandhukannan@gmail.com>
2021-09-26 15:54:04 +0530

    sam added

--- src/org/data/Data.java
+++ src/org/data/Data.java
@@ -2,7 +2,12 @@ package org.data;
 
 public class Data {
 
+	private void m3() {
+		System.out.println("Sam..");
+	}
+
 	public static void main(String[] args) {
-		
+
+		System.out.println("Sam..");
 	}
 }
