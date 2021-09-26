18d979ea1b671ee2c09517d9a5421d20ef55710b
SivaNandhini4997 <nandynandhukannan@gmail.com>
2021-09-26 15:34:54 +0530

    Anu added her code

--- src/org/data/Data.java
+++ src/org/data/Data.java
@@ -1,8 +1,13 @@
 package org.data;
 
 public class Data {
+	
+	private void m1() {
+		System.out.println("Anu method");
+	}
 
 	public static void main(String[] args) {
-		
+	
+		System.out.println("Anu..");
 	}
 }
